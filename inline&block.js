0ce7242602cfc55837967afb9188d08f05ae6b08
VenkatReddy135 <57211176+VenkatReddy135@users.noreply.github.com>
2020-12-03 14:21:42 +0530

    Create inline&block.js

---
+++ inline&block.js
@@ -0,0 +1,19 @@
+Block Means:-
+A block element always starts on a new line, 
+and fills up the horizontal space left and right on the web page. 
+You can add margins and padding on all four sides of any block element — top, right, left, and bottom.
+
+<address><article><aside><blockquote><canvas><dd><div><dl><dt><fieldset><figcaption><figure><footer><form>
+<h1>-<h6><header><hr><li><main><nav><noscript><ol><p><pre><section><table><tfoot><ul><video>
+
+Inline Means:-
+Inline elements don’t start on a new line, they appear on the same line as the content and tags beside them.
+Some examples of inline elements are <span> , <strong>, and <img> tags.
+
+<a><abbr><acronym><b><bdo><big><br><button><cite><code><dfn><em><i><img><input>
+<kbd><label><map><object><output><q><samp><script><select><small><span>
+<strong><sub><sup><textarea><time><tt><var>
+
+Inline-Block Means:-
+Inline-block elements are similar to inline elements, except they can have padding and margins added on all four sides.
+You’ll have to declare display: inline-block in your CSS code.
